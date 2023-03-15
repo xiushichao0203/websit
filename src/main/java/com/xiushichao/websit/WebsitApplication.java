92dc18ef338698c2905bced93f522440bf858bed
xiushichao <1270052467@qq.com>
2023-03-15 19:56:24 +0800

    初始化

---
+++ src/main/java/com/xiushichao/websit/WebsitApplication.java
@@ -0,0 +1,13 @@
+package com.xiushichao.websit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiushichao/websit/WebsitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiushichao.websit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
